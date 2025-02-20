dcfe8bb29c18f5a2a3809a4c45aa70cf46ab3cea
= <=>
2025-02-20 20:03:55 +0600

    INitial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-'use client'
-=======
 "use client";
->>>>>>> 4b257a82a20c2b3b0ffd09c644bfb92a4e0d86ed
+
 import AboutUs from "@/components/about-us";
 import { HomeCarousel } from "@/components/home-carousel";
 import PageLayout from "@/components/page-layout";
